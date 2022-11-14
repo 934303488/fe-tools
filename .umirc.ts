9a655daf44c4c1c04ab6c530c60dc645e5e68612
zhongcheng <zhongcheng@xigua.city>
2022-11-14 16:20:44 +0800

    适配stein,增加STEIN_METADATA标识

--- .umirc.ts
+++ .umirc.ts
@@ -5,11 +5,15 @@ export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  layout: { name: '测试工具平台', locale: false, },
+  layout: { name: '测试工具平台', locale: false },
+  scripts: ['window.FE_TESTTOOLS_METADATA=STEIN_METADATA'],
   routes: [
     { path: '/', component: '@/pages/index' },
     {
-      path: '/dashboard', name: '工具', icon: 'tool',component: '@/pages/Dashboard/Tokenboard',
+      path: '/dashboard',
+      name: '工具',
+      icon: 'tool',
+      component: '@/pages/Dashboard/Tokenboard',
       // routes: [
       //   { path: '/dashboard/monitor', icon: 'DesktopOutlined', name: '控制页', component: '@/pages/Dashboard/Monitor' },],
     },
