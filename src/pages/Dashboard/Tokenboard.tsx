3da5c6117ac34f2f2003ecb27033425848d17a4b
zhongcheng <zhongcheng@xigua.city>
2022-11-18 18:17:03 +0800

    修改服务名动态获取

--- src/pages/Dashboard/Tokenboard.tsx
+++ src/pages/Dashboard/Tokenboard.tsx
@@ -1,10 +1,19 @@
-import { Button, Card, Col, Form, FormItemProps, FormProps, Input, Row, Select } from 'antd';
+import {
+  Button,
+  Card,
+  Col,
+  Form,
+  FormItemProps,
+  FormProps,
+  Input,
+  Row,
+  Select,
+} from 'antd';
 import TextArea from 'antd/lib/input/TextArea';
-import { FormInstance } from 'antd/es/form'
-import React, { useState } from 'react';
+import { FormInstance } from 'antd/es/form';
+import React, { useCallback, useEffect, useState } from 'react';
 import styles from './Tokenboard.less';
 import { api } from './service.js';
-
 
 const { Option } = Select;
 
@@ -18,79 +27,104 @@ const tailLayout = {
   wrapperCol: { offset: 8, span: 16 },
 };
 
-
 const Page: React.FC = () => {
-  const [bearerToken,setBearerToken] = useState('');
+  const [bearerToken, setBearerToken] = useState('');
+  const [appList, setAppList] = useState([]);
   const [form] = Form.useForm();
   const [formDownload] = Form.useForm();
 
-
   const onFinish = async (values: any) => {
-    const content= await api.getBearerToken(values)
-    setBearerToken(content.data.body)
+    const content = await api.getBearerToken(values);
+    setBearerToken(content.data.body);
     console.log(content.data);
   };
 
   const createFile = async (values: any) => {
-    const content= await api.getBearerToken(values)
+    const content = await api.getBearerToken(values);
     console.log(content.data);
   };
 
-  const onReset = () => {
-    formDownload.resetFields();
-    // restValue()
+  const getAppList = async () => {
+    const content = await api.getAppList();
+    console.log(content.data.body);
+    setAppList(content.data.body);
   };
 
-  function restValue(formInstance: FormInstance){
+  function restValue(formInstance: FormInstance) {
     formInstance?.resetFields();
   }
 
   const onCopyValue = () => {
-    let txa = document.getElementById('tokenTextArea')
-    const selection = window.getSelection()
-    const range = document.createRange()
-    if (selection != null && selection.rangeCount > 0 ) {
-      selection.removeAllRanges()
-      txa!=null?range.selectNode(txa):null
-      selection.addRange(range)
+    let txa = document.getElementById('tokenTextArea');
+    const selection = window.getSelection();
+    const range = document.createRange();
+    if (selection != null && selection.rangeCount > 0) {
+      selection.removeAllRanges();
+      txa != null ? range.selectNode(txa) : null;
+      selection.addRange(range);
       // 执行浏览器复制命令
-      document.execCommand('copy')
+      document.execCommand('copy');
     }
   };
 
+  const fetchData = useCallback(() => {
+    getAppList;
+  }, []);
+
+  useEffect(() => {
+    // fetchData();
+    getAppList;
+  }, [setAppList]);
 
   return (
     <>
-      {/* <div>
-        <h1 className={styles.title}>Page Monitor</h1>
-      </div> */}
       <Row gutter={16}>
         <Col span={8}>
           <Card title="获取bearerToken" style={{ width: 400, margin: 10 }}>
-            <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
-              <Form.Item name="serviceName" label="服务名" rules={[{ required: true ,message:"请选择要获取token的服务"}]}>
+            <Form
+              {...layout}
+              form={form}
+              name="control-hooks"
+              onFinish={onFinish}
+            >
+              <Form.Item
+                name="serviceName"
+                label="服务名"
+                rules={[{ required: true, message: '请选择要获取token的服务' }]}
+              >
                 <Select
                   placeholder="选择要获取token的服务"
                   allowClear
+                  onClick={getAppList}
                 >
-                  <Option value="op-order">op-order</Option>
-                  <Option value="bfs-sms">bfs-sms</Option>
+                  {appList.map((item, index) => (
+                    <Option key={index} value={item}>
+                      {item}
+                    </Option>
+                  ))}
                 </Select>
               </Form.Item>
               <Form.Item
                 noStyle
-                shouldUpdate={(prevValues, currentValues) => prevValues.gender !== currentValues.gender}
+                shouldUpdate={(prevValues, currentValues) =>
+                  prevValues.gender !== currentValues.gender
+                }
               >
                 {({ getFieldValue }) =>
                   getFieldValue('serviceName') === 'other' ? (
-                    <Form.Item name="customizeGender" label="Customize Gender" rules={[{ required: true }]}>
+                    <Form.Item
+                      name="customizeGender"
+                      label="Customize Gender"
+                      rules={[{ required: true }]}
+                    >
                       <Input />
                     </Form.Item>
                   ) : null
                 }
               </Form.Item>
-              <Form.Item label="token" >
-                <TextArea id='tokenTextArea'
+              <Form.Item label="token">
+                <TextArea
+                  id="tokenTextArea"
                   showCount
                   maxLength={1000}
                   style={{ height: 120 }}
@@ -101,8 +135,18 @@ const Page: React.FC = () => {
                 <Button type="primary" htmlType="submit">
                   确认
                 </Button>
-                <Button htmlType='button' style={{ margin: 10 }} onClick={onCopyValue}>复制</Button>
-                <Button htmlType="reset" onClick={()=>restValue(form)} style={{ margin: 10 }}>
+                <Button
+                  htmlType="button"
+                  style={{ margin: 10 }}
+                  onClick={onCopyValue}
+                >
+                  复制
+                </Button>
+                <Button
+                  htmlType="reset"
+                  onClick={() => restValue(form)}
+                  style={{ margin: 10 }}
+                >
                   重置
                 </Button>
               </Form.Item>
@@ -110,37 +154,50 @@ const Page: React.FC = () => {
           </Card>
         </Col>
         <Col span={8}>
-          <Card title="生成导入订单文件" style={{ width: 400 ,margin:10}}>
-            <Form {...layout} form={formDownload} name="matchFile" onFinish={createFile}>
-              <Form.Item name="matchId" label="赛事id" rules={[{ required: true ,message:"请输入赛事id"}]}>
-                <Input
-                  placeholder="请输入赛事id"
-                  allowClear
-                />
+          <Card title="生成导入订单文件" style={{ width: 400, margin: 10 }}>
+            <Form
+              {...layout}
+              form={formDownload}
+              name="matchFile"
+              onFinish={createFile}
+            >
+              <Form.Item
+                name="matchId"
+                label="赛事id"
+                rules={[{ required: true, message: '请输入赛事id' }]}
+              >
+                <Input placeholder="请输入赛事id" allowClear />
               </Form.Item>
-              <Form.Item name="schemeId" label="售卖方案id" rules={[{ required: true ,message:"请输入售卖方案id"}]} >
-              <Input
-                  placeholder="请输入售卖方案id"
-                  allowClear
-                />
+              <Form.Item
+                name="schemeId"
+                label="售卖方案id"
+                rules={[{ required: true, message: '请输入售卖方案id' }]}
+              >
+                <Input placeholder="请输入售卖方案id" allowClear />
               </Form.Item>
-              <Form.Item name="fileNumber" label="文件个数" rules={[{ required: true ,message:"请输入文件个数"}]}>
-              <Input
-                  placeholder="请输入文件个数"
-                  allowClear
-                />
+              <Form.Item
+                name="fileNumber"
+                label="文件个数"
+                rules={[{ required: true, message: '请输入文件个数' }]}
+              >
+                <Input placeholder="请输入文件个数" allowClear />
               </Form.Item>
-              <Form.Item name="fileDataNumber" label="单文件数据量" rules={[{ required: true ,message:"请输入单个文件数据数量"}]}>
-              <Input
-                  placeholder="请输入单个文件数据数量"
-                  allowClear
-                />
+              <Form.Item
+                name="fileDataNumber"
+                label="单文件数据量"
+                rules={[{ required: true, message: '请输入单个文件数据数量' }]}
+              >
+                <Input placeholder="请输入单个文件数据数量" allowClear />
               </Form.Item>
               <Form.Item {...tailLayout}>
                 <Button type="primary" htmlType="submit">
                   确认
                 </Button>
-                <Button htmlType="button" onClick={()=>restValue(formDownload)} style={{ margin: 10 }}>
+                <Button
+                  htmlType="button"
+                  onClick={() => restValue(formDownload)}
+                  style={{ margin: 10 }}
+                >
                   重置
                 </Button>
               </Form.Item>
@@ -149,8 +206,7 @@ const Page: React.FC = () => {
         </Col>
       </Row>
     </>
-    
   );
-}
+};
 
 export default Page;
--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,40 +1,39 @@
-
-
 export default {
   fileUpload: '/file/upload',
   getMusic: '/music/list',
 };
 
-export const TOOLS_API={
-  getBearerToken:'/getBearerToken',
-}
+export const TOOLS_API = {
+  getBearerToken: '/getBearerToken',
+  getAppList: '/appList',
+};
 
-export const ORDER_API={
-  orderLogisticsChange:'/order/logistics-change',
+export const ORDER_API = {
+  orderLogisticsChange: '/order/logistics-change',
   orderList: '/order/distributor-order/list',
-}
+};
 
-export const DIRECTORY_API={
-  directoryPage:'/directory/page',
-  directoryUpsert:'/directory/upsert',
-}
+export const DIRECTORY_API = {
+  directoryPage: '/directory/page',
+  directoryUpsert: '/directory/upsert',
+};
 
-export const DRAW_API={
-  luckDraw:'/lucky-draw/do/lucky-draw',
-  pondList:'/lucky-draw/candidates/list',
+export const DRAW_API = {
+  luckDraw: '/lucky-draw/do/lucky-draw',
+  pondList: '/lucky-draw/candidates/list',
   luckDrawResult: '/lucky-draw/get/lucky-draw-result',
   lateRecordsList: '/lucky-draw/get/late-records/list',
   addLateRecords: '/lucky-draw/do/add/late-records',
   editPonds: '/lucky-draw/do/update-status',
-  addPonds: '/private/lucky-draw/import-candidates'
-}
+  addPonds: '/private/lucky-draw/import-candidates',
+};
 
-export const NODE_API={
-  nodeUpdate:'/node/update',
-  nodeTree:'/node/tree'
-}
+export const NODE_API = {
+  nodeUpdate: '/node/update',
+  nodeTree: '/node/tree',
+};
 
-export const CRYPTO_API={
-  encryption:'/encryption/value/get',
-  decryption:'/decryption/value/get'
-}
+export const CRYPTO_API = {
+  encryption: '/encryption/value/get',
+  decryption: '/decryption/value/get',
+};
