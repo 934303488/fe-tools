49dd4fc97959270731fb6f2c25c7346dbdd97a4c
zhongcheng <zhongcheng@xigua.city>
2023-03-31 11:31:37 +0800

    添加message

--- src/pages/Dashboard/components/TimeLineSync.tsx
+++ src/pages/Dashboard/components/TimeLineSync.tsx
@@ -1,33 +1,20 @@
-import { Alert, Button, Card, Col, Form, Input, Select } from 'antd';
-import React, { useState } from 'react';
+import { Button, Card, Col, Form, Input, Select, message } from 'antd';
+import React from 'react';
 import { api } from '../service';
 
 const TimeLineSync: React.FC = () => {
   const [form] = Form.useForm();
-  // const [alertDisplay, setAlertDisplay] = useState('');
-
-  // const showAlert =(value: string)=>{
-  //   document.getElementById('showAlert').style.display=value
-  // }
-
   const onFinish = async (values: any) => {
     const content = await api.timeLineSync(values);
     console.log(content.data.success);
     if ((content.data.success = true)) {
-      alert('同步成功');
+      message.success('同步成功');
     }
   };
 
   return (
     <Col span={8}>
       <Card title="同步IM消息到timeline" style={{ width: 400, margin: 10 }}>
-        {/* <div id ='showAlert' style={{display:'none',width: 120,marginLeft:100}}>
-          <Alert
-            message="同步成功" 
-            type="success"
-            showIcon
-          /> 
-        </div> */}
         <Form form={form} name="control-hooks" onFinish={onFinish}>
           <Form.Item
             label="消息id"
